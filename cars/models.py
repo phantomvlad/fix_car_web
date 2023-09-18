2fdc02125f20e86efd72757fcdf2387368cd4e88
PhantomVlad <mmvlad99@mail.ru>
2023-09-18 20:08:48 +0300

    add verbose name to car model

--- cars/models.py
+++ cars/models.py
@@ -3,18 +3,19 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 class Car(models.Model):
-    brand = models.CharField(verbose_name = 'brand')
-    model = models.CharField()
-    generation = models.IntegerField()
-    transmission = models.CharField()
-    engine = models.CharField()
-    air = models.CharField()
-    gaz = models.BooleanField()
-    wheel_drive = models.CharField()
-    capacity = models.IntegerField()
-    mileage = models.IntegerField()
-    vincode = models.CharField()
-    owner = models.ForeignKey(get_user_model(), on_delete = models.SET_NULL, related_name='cars', null=True)
+    brand = models.CharField(verbose_name='Марка')
+    model = models.CharField(verbose_name='Модель')
+    photo = models.ImageField(default=None, verbose_name='Изображение авто')
+    generation = models.IntegerField(verbose_name='Год выпуска')
+    transmission = models.CharField(verbose_name='Тип коробки передач')
+    engine = models.CharField(verbose_name='Тип двигателя')
+    air = models.CharField(verbose_name='Подача воздуха')
+    gaz = models.BooleanField(verbose_name='Газовое оборудование')
+    wheel_drive = models.CharField(verbose_name='Привод')
+    capacity = models.IntegerField(verbose_name='Мощность двигателя')
+    mileage = models.IntegerField(verbose_name='Пробег')
+    vincode = models.CharField(verbose_name='Винкод')
+    owner = models.ForeignKey(get_user_model(), on_delete = models.SET_NULL, related_name='cars', null=True, verbose_name = 'Владелец')
 
 
     def __str__(self):
