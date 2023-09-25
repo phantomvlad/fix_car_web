79c1a0771e3c0a3a65f0a41021ce675348edaf77
PhantomVlad <mmvlad99@mail.ru>
2023-09-25 13:42:30 +0300

    add get_absolute_url in user

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
@@ -6,3 +7,6 @@ class CustomUser(AbstractUser):
     slug = models.SlugField(unique=True, max_length=255, db_index=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('account_show', kwargs={'slug': self.slug})
